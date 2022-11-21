b520a3953027d5cccdd009abadd5010657bf5604
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-11-21 21:20:57 +0900

    annotate導入してrouteとmodelに柱脚

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint           not null, primary key
+#  name        :string(100)      not null
+#  start       :datetime         not null
+#  end         :datetime         not null
+#  timed       :boolean          default(TRUE)
+#  description :text(65535)
+#  color       :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Event < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :start, presence: true
