3f0b57005e95407351403425bbada97fe1087042
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-11-25 08:38:43 +0900

    valueの値になっている日付をカレンダーに表示する＆日本語対応

--- frontend/src/components/DateCalendar.vue
+++ frontend/src/components/DateCalendar.vue
@@ -1,24 +1,49 @@
 <template>
   <div>
-    <v-sheet height="100vh">
-      <v-calendar v-model="value" :events="events" @change="fetchEvents"></v-calendar>
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-btn outlined small class="ma-4" @click="setToday">今日</v-btn>
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="(timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'"
+      ></v-calendar>
     </v-sheet>
   </div>
 </template>
 
 <script>
+import { format } from "date-fns";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "DateCalendar",
   data: () => ({
-    value: new Date("2022/11/01"), // 表示する月を指定
+    value: format(new Date(), "yyyy/MM/dd"),
   }),
   computed: {
     ...mapGetters("events", ["events"]),
+    title() {
+      return format(new Date(this.value), "yyyy年 M月");
+    },
   },
   methods: {
     ...mapActions("events", ["fetchEvents"]),
+    setToday() {
+      this.value = format(new Date(), "yyyy/MM/dd");
+    },
   },
 };
 </script>
