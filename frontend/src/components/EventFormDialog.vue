1f645348f8da08f2ce443db9062b0e7ee00e4aa1
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-11-27 08:27:23 +0900

    日付の選択フォームを追加

--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -10,28 +10,56 @@
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
     </v-card-text>
+    <v-card-text>
+      <DialogSection icon="mdi-clock-outline">
+        <DateForm v-model="startDate" />
+        <DateForm v-model="endDate" />
+      </DialogSection>
+    </v-card-text>
+    <v-card-actions class="d-flex justify-end">
+      <v-btn @click="submit">保存</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 import DialogSection from "./DialogSection";
+import DateForm from "./DateForm";
+import { format } from "date-fns";
 
 export default {
   name: "EventFormDialog",
   components: {
     DialogSection,
+    DateForm,
   },
   data: () => ({
     name: "",
+    startDate: null,
+    endDate: null,
   }),
   computed: {
     ...mapGetters("events", ["event"]),
   },
+  created() {
+    this.startDate = format(this.event.start, "yyyy/MM/dd");
+    this.endDate = format(this.event.end, "yyyy/MM/dd");
+  },
   methods: {
-    ...mapActions("events", ["setEvent"]),
+    ...mapActions("events", ["setEvent", "setEditMode", "createEvent"]),
     closeDialog() {
+      this.setEditMode(false);
       this.setEvent(null);
+    },
+    submit() {
+      const params = {
+        name: this.name,
+        start: this.startDate,
+        end: this.endDate,
+      };
+      this.createEvent(params);
+      this.closeDialog();
     },
   },
 };
